e4dd94350ad80db4aaa7c8a88900f50cf6104b30
ajinkyachavan <a.ajinkya9030@gmail.com>
2021-12-22 00:29:36 -0500

    chore: set up base modules. add colors for custom theme. add tailwind css. add page-not-found route

--- rxjs-ide/src/app/app.module.ts
+++ rxjs-ide/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { MaterialModule } from './material.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    // Angular modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // Custom modules
+    MaterialModule,
+
+    // Code modules
+    SharedModule,
+    HeaderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
